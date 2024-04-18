dca4cb2f0eb29ab04806b0d5c0eeded590153ae7
msri-02 <msrikant@calpoly.edu>
2024-04-18 00:04:47 -0700

    idek

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -117,7 +117,7 @@ app.listen(port, () => {
 app.post("/users", (req, res) => {
   const userToAdd = req.body;
   addUser(userToAdd);
-  res.status(201).send("Successfully added user");
+  res.status(201).send(userToAdd);
   //res.send();
 });
 
